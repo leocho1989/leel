22c743005223fd4215981878bec1794a1f9a563f
Leo <leo.cho1989@gmail.com>
2021-07-03 15:58:37 -0700

    password fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
     attr_reader :password
 
     validates :username, presence: true, uniqueness: true
-    validates :password_digest, presence: true, length: {minimum: 6}, allow_nil: true
+    validates :password_digest, presence: true
+    validates :password, length: {minimum: 6, allow_nil: true}
     validates :session_token, presence: true
 
     after_initialize :ensure_session_token
