65e21bf49d16d79ec30bfbde7c4e1a036bcc0e8e
Leo <leo.cho1989@gmail.com>
2021-07-07 09:22:04 -0700

    last auth fix

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,65 +1,54 @@
-class UsersController < ApplicationController
-    def new
-        @user = User.new
-        render :new
-    end
+class Api::UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
         if @user.save
             login!(@user)
-            redirect_to user_url(@user)
+            render 'api/users/show'
         else
-            flash.now[:errors] = @user.errors.full_messages
-            render :new
+            render json: @user.errors.full_messages,
+            status: 400
         end
     end
 
     def show
-        @user = User.find_by(params[:id])
-        if @user
-        render :show
-        else
-            render json: @user.errors.full_messages,status: 404
-        end
+        @user = selected_user
     end
 
     def index
         @users = User.all
-        render :index
     end
 
-    def edit
-        @user = User.find(params[:id])
-        render :edit
-    end
-        
     def update
-        @user = User.find(params[:id])
-        if @user.update(user_params)
-            redirect_to user_url(@user)
-        else
-            render json: @user.errors.full_messages,status: 422
+        @user = selected_user
+        if @user && @user.update_attributes(user_params)
+            render 'api/users/show'
+        elsif
+            !@user
+            render json: ['Invalid username'],
+            status: 400
+        else render json: @user.errors.full_messages,
+            status: 401
         end
     end
 
     def destroy
-        @user = User.find(params[:id])
-        if @user.destroy
-            redirect_to users_url
+        @user = selected_user
+        if @user
+            @user.destroy
+            render 'api/users/show'
         else
-            render json: @user.errors.full_messages,status:422
+            render json: ['Invalid username']
         end
-    end
-    
-    def search
-        @users = User.where("username LIKE '%#{params[:query]}%'")
-        render json: @users
     end
 
     private
+    def selected_user
+        User.find(params[:id])
+    end
 
     def user_params
-        params.require(:user).permit(:username, :password, :email)
-    end       
+        params.require(:user).permit(:username, :password,:email)
+    end
+        
 end
