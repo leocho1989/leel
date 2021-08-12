6356660edb0bc7572ee84ababbe6f7deaab3776c
Leo <leo.cho1989@gmail.com>
2021-08-12 12:41:54 -0700

    added linkedin

--- frontend/components/nav_bar/footer.jsx
+++ frontend/components/nav_bar/footer.jsx
@@ -8,7 +8,7 @@ export default ()=>{
 return (
 
 <footer className="bottom">
-    &nbsp;<a target="_blank" href="https://github.com/leocho1989"><GithubOutlined /></a>&nbsp;&nbsp;<LinkedinFilled />&nbsp;&nbsp;<a target="_blank" href="mailto:leocho1989@gmail.com"><MailFilled /></a>&nbsp;&nbsp;
+    &nbsp;<a target="_blank" href="https://github.com/leocho1989"><GithubOutlined /></a>&nbsp;&nbsp;<a target="_blank" href="https://www.linkedin.com/in/leocho1989/"><LinkedinFilled /></a>&nbsp;&nbsp;<a target="_blank" href="mailto:leocho1989@gmail.com"><MailFilled /></a>&nbsp;&nbsp;
 <p>Powered by Leo Cao&nbsp;&nbsp;</p>
 </footer>
 
